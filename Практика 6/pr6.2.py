db662007d1a9eac93f4ea114de676bdef0dfcc69
Tackleboxs <145257838+Tackleboxs@users.noreply.github.com>
2023-10-18 22:19:07 +0300

    Add files via upload

---
+++ Практика 6/pr6.2.py
@@ -0,0 +1,20 @@
+# # 2 вариант
+
+# # 1 задание
+# n = [7,3,5,2,4,3,9,6,6]
+# print(min(n))
+# print(n.index(min(n)))
+
+# # 2 задание
+# import random
+# a = [random.randint(-10,10) for i in range(10)]
+# b = []
+# c = []
+# for i in a:
+#     if i >= 0:
+#         b.append(i)
+#     else:
+#         c.append(i)
+# print(a)
+# print(b)
+# print(c)
