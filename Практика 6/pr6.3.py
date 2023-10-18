1bf421c6d1f5a87f90c227d36079e90abc87137f
Tackleboxs <145257838+Tackleboxs@users.noreply.github.com>
2023-10-18 22:21:01 +0300

    Add files via upload

---
+++ Практика 6/pr6.3.py
@@ -0,0 +1,20 @@
+# # 3 вариант
+#
+# # 1 задание
+# import random
+# a = [random.randint(-10,10) for i in range(10)]
+# c = []
+# for i in a:
+#     if i < 0:
+#         c.append(i)
+# print(a)
+# print(sum(c))
+
+# # 2 задание
+# n = [10, 20, 5, 8, 25, 12, 16, 3]
+#
+# for i in range(len(n)):
+#     if n[i] < 15:
+#         n[i] = n[i] * 2
+#
+# print(n)
