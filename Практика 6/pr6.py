b1e2ddf18a28bf0cab6c57a7cc15be416388a5dc
Tackleboxs <145257838+Tackleboxs@users.noreply.github.com>
2023-10-18 22:16:50 +0300

    Add files via upload

---
+++ Практика 6/pr6.py
@@ -0,0 +1,13 @@
+# # 1 вариант
+
+# # 1 задание
+# import random
+# n = [1,9,3,9,2,4,7,11]
+# print(n[::-1])
+# print(max(n))
+
+# # 2 задание
+# n = [1,4,3,0,0,9]
+# s = sum(n) / len(n)
+# n = [s if i == 0 else i for i in n]
+# print(n)
