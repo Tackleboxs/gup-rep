49a7c9d73682b8c8acab5d5d633c61ea3f75c12b
Tackleboxs <145257838+Tackleboxs@users.noreply.github.com>
2023-11-26 19:17:46 +0300

    Delete Практика 10 directory

--- Практика 10/pr10.py
+++
@@ -1,6 +0,0 @@
-with open('Гупалов Владислав Александрович_УБ-31_Ввод.txt', 'r') as file:
-    data = file.readlines()
-    new = str(data)
-
-with open('Гупалов Владислав Александрович_УБ-31_Вывод.txt', 'w') as file2:
-    file2.write(new)
